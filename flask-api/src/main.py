821c5bbfff83be3c29e882c56ea4e29a9cc64665
Rohit_Rai <rohitrai1310@gmail.com>
2026-07-19 17:46:01 +0530

    Initialize Flask application with hello route

---
+++ flask-api/src/main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+print(f"Starting application with ENV app_version: {os.getenv('app_version')}")
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    
+    app.run(host="0.0.0.0", port=5000)
